a59b1762ec9b397d5eddf3bc13f1778922a1e244
k-ishida <isis331@gmail.com>
2013-03-28 12:55:24 +0900

    use TiBaseActivity#launchActivityForResult() instead of startActivityForResult()

--- src/org/mumumu/ti/android/speech/VoiceRecognitionProxy.java
+++ src/org/mumumu/ti/android/speech/VoiceRecognitionProxy.java
@@ -2,33 +2,72 @@ package org.mumumu.ti.android.speech;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import org.appcelerator.kroll.KrollDict;
 import org.appcelerator.kroll.KrollFunction;
 import org.appcelerator.kroll.KrollProxy;
 import org.appcelerator.kroll.annotations.Kroll;
 import org.appcelerator.kroll.common.Log;
+import org.appcelerator.titanium.TiApplication;
+import org.appcelerator.titanium.util.TiActivityResultHandler;
 
 import android.app.Activity;
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.os.Messenger;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.speech.RecognizerIntent;
 
 //
 // This proxy can be created by calling Voicerecognition.createVoiceRecognition()
 //
 @Kroll.proxy(creatableInModule=VoicerecognitionModule.class)
-public class VoiceRecognitionProxy extends KrollProxy
+public class VoiceRecognitionProxy extends KrollProxy implements TiActivityResultHandler
+
 {
     
     private static final String TAG = "VoiceRecognitionProxy";
     // callback function for getting Voice Recognition result
     private KrollFunction callback = null;
 
+    private static final int VOICE_RECOGNITION_REQUEST_CODE = 1234;
+ 
+    /**
+     * Fire an intent to start the speech recognition activity.
+     */
+    private void startVoiceRecognitionActivity(KrollDict options) {
+    	Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+    	
+    	//
+    	//  overriding option value
+    	//
+        if (options != null) {
+        	if (options.containsKey(RecognizerIntent.EXTRA_LANGUAGE_MODEL)) {
+        		String extraVal = (String)options.get(RecognizerIntent.EXTRA_LANGUAGE_MODEL);
+        		Log.d(TAG, "overriding RecognizerIntent.EXTRA_LANGUAGE_MODEL value -> " + extraVal);
+            	intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, extraVal);
+            }
+            if (options.containsKey(RecognizerIntent.EXTRA_PROMPT)) {
+            	String extraVal = (String)options.get(RecognizerIntent.EXTRA_PROMPT);
+            	Log.d(TAG, "overriding RecognizerIntent.EXTRA_PROMPT value");
+               	intent.putExtra(RecognizerIntent.EXTRA_PROMPT, extraVal);
+            }
+        }
+        TiApplication.getInstance().getRootActivity().launchActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE, this);
+    }
+    
+    private void sendResult(ArrayList<String> matches, boolean enabled, boolean canceled) {
+        if (matches == null) {
+            matches = new ArrayList<String>();
+        }
+       HashMap<String,Object> resultmap = new HashMap<String,Object>();
+        String[] result_string_array = matches.toArray(new String[0]);
+        resultmap.put("voice_results", result_string_array);
+        resultmap.put("voice_enabled", enabled);
+        resultmap.put("voice_canceled", canceled);
+        callback.callAsync(getKrollObject(), resultmap);    
+    }
+ 
     @Kroll.setProperty @Kroll.method
     public void setCallback(KrollFunction func) {
         this.callback = func;
@@ -39,13 +78,6 @@ public class VoiceRecognitionProxy extends KrollProxy
     public void voiceRecognition(KrollDict options) {    
         
     	Log.d(TAG, "Voice Recognition entry point");
-        Activity currentActivity = getActivity();
-        Intent intent = new Intent(
-                            currentActivity.getApplicationContext(),
-                            VoiceRecognitionActivity.class
-                        );
-        intent.setAction(Intent.ACTION_VIEW);
-        
         //
         //  You can override callback functions.
         //
@@ -55,55 +87,41 @@ public class VoiceRecognitionProxy extends KrollProxy
         	Log.d(TAG, "overriding callback value");
         	callback = (KrollFunction)options.get("callback");
         }
-        
-        //
-        //  activity result handling
-        //
-        Handler handler = new Handler() {
-            public void handleMessage(Message msg) {
-                Log.d(TAG, "Voice Recognition result handled.");
-                if (callback != null) {
-                    Bundle bundle = msg.getData();
-                    ArrayList<String> matches = bundle.getStringArrayList("VOICE_RESULT");
-                    for (int i = 0; i < matches.size(); i++) {
-                        Log.d(TAG, " Recognize result -> " + matches.get(i));
-                    }
-                    HashMap resultmap = new HashMap();
-                    String[] result_string_array = matches.toArray(new String[0]);
-                    resultmap.put("voice_results", result_string_array);
-                    resultmap.put("voice_enabled", bundle.getBoolean("VOICE_RECOGNITION_ENABLED"));
-                    resultmap.put("voice_canceled", bundle.getBoolean("VOICE_RECOGNITION_CANCELED"));
-                    callback.callAsync(getKrollObject(), resultmap);    
-                }
-            }
-        };
-        
-        //
-        //    invoke main Activity.
-        //
-        Messenger msger = new Messenger(handler);
-        intent.putExtra("VOICE_RESULT_MESSENGER", msger);
-        if (options != null) {
-        	if (options.containsKey(RecognizerIntent.EXTRA_LANGUAGE_MODEL)) {
-        		String extraVal = (String)options.get(RecognizerIntent.EXTRA_LANGUAGE_MODEL);
-        		Log.d(TAG, "overriding RecognizerIntent.EXTRA_LANGUAGE_MODEL value -> " + extraVal);
-        		intent.putExtra("android.speech.extra.LANGUAGE_MODEL", extraVal);
-            }
-            if (options.containsKey(RecognizerIntent.EXTRA_PROMPT)) {
-            	String extraVal = (String)options.get(RecognizerIntent.EXTRA_PROMPT);
-            	Log.d(TAG, "overriding RecognizerIntent.EXTRA_PROMPT value");
-            	intent.putExtra(RecognizerIntent.EXTRA_PROMPT, extraVal);
-            }
-        }
-        try {
-            currentActivity.startActivity(intent);
-        } catch (ActivityNotFoundException e) {
-            Log.e(TAG, "VoiceRecognitionActivity not found, check your activity setting in tiapp.xml");
-            HashMap resultmap = new HashMap();
-            resultmap.put("voice_results", new ArrayList<String>());
-            resultmap.put("voice_enabled", false);
-            resultmap.put("voice_canceled", false);
-            callback.callAsync(getKrollObject(), resultmap);
+        // Check to see if a recognition activity is present
+        PackageManager pm = TiApplication.getInstance().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(
+                new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
+        boolean isVoiceRecognitionEnabled = (activities.size() != 0);
+        if (isVoiceRecognitionEnabled) {
+            startVoiceRecognitionActivity(options);
+        } else {
+            sendResult(null, isVoiceRecognitionEnabled, false);
         }
     }
+
+   
+
+    @Override
+	public void onError(Activity arg0, int arg1, Exception arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+ 	@Override
+	public void onResult(Activity activity, int requestCode, int resultCode, Intent data) {
+        if (requestCode == VOICE_RECOGNITION_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                // Fill the list view with the strings the recognizer thought it could have heard
+                ArrayList<String> matches = data.getStringArrayListExtra(
+                        RecognizerIntent.EXTRA_RESULTS);
+                Log.d("VoiceRecognitionActivity", "recognition result received");
+                for (int i = 0; i < matches.size(); i++) {
+                    Log.d("VoiceRecognitionActivity", matches.get(i));
+                }
+                sendResult(matches, true, false);
+            }else if (resultCode == Activity.RESULT_CANCELED) {
+                sendResult(null, true, true);                
+            }
+        }
+	}
 }
